d76525c1132c71f6f8cfd6936ad3b87e82cd259f
kou-colour <kou@aiit.ac.jp>
2015-06-06 11:06:18 +0900

    kadai3

--- test.bash
+++ test.bash
@@ -9,9 +9,9 @@ ERROR_EXIT () {
 }
 
 
-echo 84 273 21 > $tmp-ans
+echo 21 > $tmp-ans1
 ./divisor.bash 84 273 > $tmp-out || ERROR_EXIT "TEST1-1"
-diff $tmp-ans $tmp-out || ERROR_EXIT "TEST1-2"
+diff $tmp-ans1 $tmp-out || ERROR_EXIT "TEST1-2"
 
 echo OK
 rm -f $tmp-*
